f9168757d4b17951ccc6775c21eef94c9faaed26
alhasann351 <alhasann352@gmail.com>
2024-03-26 01:36:09 +0600

    linear-gradient-added

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
+      /*appBar: AppBar(
         backgroundColor: Colors.blue,
         title: const Text(
           'Gradient Background',
@@ -37,15 +37,21 @@ class _MyHomePageState extends State<MyHomePage> {
             color: Colors.white,
           ),
         ),
-      ),
-      body: const Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Gradient Background',
-            ),
-          ],
+      ),*/
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color(0xFF84fab0),
+              Color(0xFF8fd3f4),
+              Colors.green,
+            ],
+            begin: FractionalOffset(1.0, 0.0),
+            end: FractionalOffset(0.0, 1.0),
+            stops: [0.0, 0.5, 0.9],
+          ),
         ),
       ),
     );
